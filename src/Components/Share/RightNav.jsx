7a0507a1ef9a6db618a921ddcef83adefa04711e
Rakibul Islam <rakib011110@gmail.com>
2023-04-24 20:50:10 +0600

    right Nav add

--- src/Components/Share/RightNav.jsx
+++ src/Components/Share/RightNav.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Button, ListGroup } from "react-bootstrap";
 
 const RightNav = () => {
   return (
     <div>
-      <h1> Roght Nav</h1>
+      <h4 className="mt-4">Login With</h4>
+      <Button className="mb-2" variant="outline-primary">
+        {" "}
+        <FaGoogle /> Login with Google
+      </Button>
+      <Button variant="outline-secondary">
+        {" "}
+        <FaGithub></FaGithub> Login with Github
+      </Button>
+      <div>
+        <h4 className="mt-4">Find us on</h4>
+        <ListGroup>
+          <ListGroup.Item>
+            {" "}
+            <FaFacebook></FaFacebook> facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            {" "}
+            <FaTwitter></FaTwitter> twitter{" "}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram></FaInstagram> instagram
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <QZone></QZone>
+      <div>
+        <img src={bg} alt="" />
+      </div>
     </div>
   );
 };
