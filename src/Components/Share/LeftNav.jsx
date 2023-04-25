966e653b7326b43bdc299789bd5797e88ffa218a
Rakibul Islam <rakib011110@gmail.com>
2023-04-25 20:28:46 +0600

    categories add

--- src/Components/Share/LeftNav.jsx
+++ src/Components/Share/LeftNav.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
+  const [categories, setCategories] = useState([]);
+  console.log(categories);
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
-      <h1> Here is left nav</h1>
+      <h1> All Category</h1>{" "}
+      <div className="p-4  ">
+        {categories.map((category) => (
+          <p key={category.id}>
+            {" "}
+            <Link
+              to={`category/${category.id}`}
+              className="text-decoration-none text-black fw-bold"
+            >
+              {" "}
+              {category.name}{" "}
+            </Link>{" "}
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
