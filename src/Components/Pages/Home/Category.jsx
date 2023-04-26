4fef3fd038bd367250dbb0a2b0947b8a56d100cc
Rakibul Islam <rakib011110@gmail.com>
2023-04-26 17:43:49 +0600

    news card add

--- src/Components/Pages/Home/Category.jsx
+++ src/Components/Pages/Home/Category.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import NewsCard from "./NewsCard";
 
 const Category = () => {
   const categoryNews = useLoaderData();
@@ -10,6 +11,12 @@ const Category = () => {
   return (
     <div>
       <h1>Catorgy news are {categoryNews.length} </h1>
+
+      <div>
+        {categoryNews.map((news) => (
+          <NewsCard news={news} key={news._id}></NewsCard>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Layout/NewsLayOuts.jsx
+++ src/Layout/NewsLayOuts.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Header from "../Components/Share/Header";
 import { Outlet } from "react-router-dom";
+import RightNav from "../Components/Share/RightNav";
 
 const NewsLayOuts = () => {
   return (
